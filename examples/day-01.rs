f0cad704ef310bc641c442707903838d08593e57
Chris Oakley <chris.oakley@redcanary.com>
2022-12-01 14:56:58 -0700

    pragmatic approaches to day 1.

--- examples/day-01.rs
+++ examples/day-01.rs
@@ -1,22 +1,19 @@
 use std::fs;
 
-const TOP_ELVES: i32 = 3;
+const TOP_ELVES: usize = 3;
 
 fn main() {
-    let mut total_max_calories = 0;
     let file_contents = read_file("./examples/day-01-input.txt");
 
     let mut total_calories_per_elf_map: Vec<i32> = retrieve_calories_per_elf(file_contents);
 
-    (0..TOP_ELVES).for_each(|_i| {
-        let max_calories = total_calories_per_elf_map.iter().max().unwrap();
-        let elf_index = total_calories_per_elf_map
-            .iter()
-            .position(|v| v == max_calories)
-            .unwrap();
+    total_calories_per_elf_map.sort();
 
-        total_max_calories += total_calories_per_elf_map.remove(elf_index);
-    });
+    let total_max_calories: i32 = total_calories_per_elf_map
+        .into_iter()
+        .rev()
+        .take(TOP_ELVES)
+        .sum();
 
     println!("Max Calories: {total_max_calories}")
 }
@@ -34,22 +31,11 @@ fn read_file(path: &str) -> String {
 fn retrieve_calories_per_elf(input: String) -> Vec<i32> {
     input
         .split("\n\n")
-        .enumerate()
-        .map(|(_elf_index, elf_inventory)| {
-            let mut elf_calories = 0;
+        .map(|elf_inventory| {
             elf_inventory
-                .split("\n")
-                .enumerate()
-                .for_each(|(_i, item_calorie)| {
-                    let parsed_item_calorie = match item_calorie.parse::<i32>() {
-                        Ok(v) => v,
-                        Err(_) => 0,
-                    };
-
-                    elf_calories += parsed_item_calorie
-                });
-
-            elf_calories
+                .lines()
+                .map(|item_calorie| item_calorie.parse::<i32>().unwrap())
         })
+        .map(|calories| calories.sum())
         .collect()
 }
